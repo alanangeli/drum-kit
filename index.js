ff5523851f85a5aff2a036864db75581892b13d3
Alan <71236097+alanangeli@users.noreply.github.com>
2023-03-24 12:58:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,27 @@
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert("Eu fui clicado!");
-    });
-}
+        
+        var buttonInnerHtml = this.innerHtml;
 
+        switch (buttonInnerHtml) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break; 
+
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom3.play();
+                break;
+                
