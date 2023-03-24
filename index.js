ee967889e47e22c52bbab82c59b979ea3b370f79
Alan <71236097+alanangeli@users.noreply.github.com>
2023-03-24 10:29:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert("maoe00");
+//document.querySelector("button").addEventListener("click", handleClick);
+//
+//function handleClick() {
+//    alert("Eu fui clicado")
+//}
+
+//** Uma forma seria chamar a função handleClick. Outra forma, é criando uma função anônima ao invés de chamar a função
+//handleClick **
+
+//document.querySelector("Button").addEventListener("click", function(){
+//    alert ("Eu fui clicado!");
+//});
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("Eu fui clicado!");
+    });
+}
+
